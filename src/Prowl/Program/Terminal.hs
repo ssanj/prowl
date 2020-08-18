7b6751a32c63e5dceca035f352b4594fb49ca343
sanjiv sahayam <sanjsmailbox@gmail.com>
2020-08-18 16:28:48 +1000

    Fix terminal output

--- src/Prowl/Program/Terminal.hs
+++ src/Prowl/Program/Terminal.hs
@@ -11,6 +11,7 @@ module Prowl.Program.Terminal
 
 import System.Exit         (ExitCode(..))
 import System.Directory    (createDirectoryIfMissing)
+import Control.Monad       (void)
 
 import qualified Prowl.Config.Model  as C
 import qualified Prowl.Common.Model  as C
@@ -19,8 +20,8 @@ import qualified System.Process      as P
 import qualified Data.Text           as T
 import qualified Data.Text.IO        as T
 
-newtype Command = Command String
-newtype Args = Args [String]
+newtype Command = Command T.Text
+newtype Args = Args [T.Text]
 
 gitClone :: C.ProwlWorkDir -> C.GithubDomain -> C.PullRequest -> IO ()
 gitClone workingDir domain pr = do
@@ -31,10 +32,13 @@ gitClone workingDir domain pr = do
       branch   = C._prowlPullRequestBranchValue . C._prowlPullRequestDetailBranch . C._prowlPullRequestDetail $ pr
       ghcUrl    = C.unmkTextTag . C.getGithubCloneUrl $ domain -- C.unmkTextTag .  C._prowlPullRequestDetailApi . C._prowlPullRequestDetail $ pr
 
-      cloneUrl = joinDirectories "/" [ghcUrl, org, (repo <> ".git")]
-      cloneDir = joinDirectories "/" [wkdir, "clones", org, repo, branch, hash]
-  -- _ <- runCommandF (Command "git") (Args ["clone", cloneUrl, "-b", branch, cloneDir])
-  T.putStrLn $ "git clone " <> cloneUrl <> " -b " <> branch <> " " <> cloneDir
+      clonePath = joinDirectories "/" [ghcUrl, org, repo]
+      cloneUrl  = clonePath <> ".git"
+      cloneDir  = joinDirectories "/" [wkdir, "clones", org, repo, branch, hash]
+  T.putStrLn $ "git cloning from " <> clonePath <> ":" <> branch <> " -> " <> cloneDir
+  void $ runCommandF (Command "git") (Args ["clone", cloneUrl, "-b", branch, cloneDir])
+  -- TODO: Run a script based on a heuristic
+  -- Accept a function to run after cloning, with the clone directory path as input
 
 joinDirectories :: T.Text -> [T.Text] -> T.Text
 joinDirectories = T.intercalate
@@ -43,11 +47,11 @@ mkDir :: C.ProwlWorkDir -> IO ()
 mkDir = createDirectoryIfMissing True .  T.unpack . C._prowlWorkDirLocation
 
 runCommandF :: Command -> Args -> IO T.Text
-runCommandF (Command command) (Args args) = T.pack <$> (P.readProcess command args "")
+runCommandF (Command command) (Args args) = T.pack <$> (P.readProcess (T.unpack command) (fmap T.unpack args) "")
 
 runCommand :: Command -> Args -> IO (Maybe [T.Text])
 runCommand (Command command) (Args args) = do
-  (exitCode, stdout, _) <- P.readProcessWithExitCode command args ""
+  (exitCode, stdout, _) <- P.readProcessWithExitCode (T.unpack command) (fmap T.unpack args) ""
   case exitCode of
     ExitSuccess     -> pure . Just . fmap T.pack . lines $ stdout
     (ExitFailure _) -> pure Nothing
